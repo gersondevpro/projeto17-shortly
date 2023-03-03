28deeb3ba18eb40387514b5b9832a12c7306964d
GersonGoncalves <gerson.devpro@gmail.com>
2023-03-03 03:18:10 -0300

    alteração dump

--- dump.sql
+++ dump.sql
@@ -29,7 +29,8 @@ CREATE TABLE public.urls (
     "shortUrl" character varying(8) NOT NULL,
     url character varying(355) NOT NULL,
     "visitCount" integer DEFAULT 0 NOT NULL,
-    id_user integer NOT NULL
+    id_user integer NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
@@ -62,7 +63,8 @@ CREATE TABLE public.users (
     name character varying(255) NOT NULL,
     email character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
-    token text
+    token text,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
@@ -104,36 +106,34 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.urls VALUES (1, 'M8w6Sya0', 'http://www.maisum.com.br', 5, 1);
-INSERT INTO public.urls VALUES (3, 'qxuzBDw9', 'http://www.sbt.com.br', 4, 5);
-INSERT INTO public.urls VALUES (2, 'KZmCMkZE', 'http://www.globo.com', 9, 2);
-INSERT INTO public.urls VALUES (4, 'AR-12EA6', 'http://www.record.com.br', 3, 4);
-INSERT INTO public.urls VALUES (5, 'h0qYkQ-q', 'http://www.google.com.br', 17, 2);
+INSERT INTO public.urls VALUES (1, '1Wyh7I0E', 'https://www.facebook.com', 5, 1, '2023-03-03 02:12:45.432929');
+INSERT INTO public.urls VALUES (2, 'IR-L2WKx', 'https://www.google.com', 11, 1, '2023-03-03 02:20:58.080746');
+INSERT INTO public.urls VALUES (3, 'gwuELJK_', 'https://www.safari.com', 4, 1, '2023-03-03 02:21:04.130223');
+INSERT INTO public.urls VALUES (4, 'jaE_qJ_D', 'https://www.globo.com', 18, 2, '2023-03-03 02:22:59.072176');
+INSERT INTO public.urls VALUES (5, 'ARcnxFW5', 'https://www.samsumg.com', 2, 2, '2023-03-03 02:23:14.175565');
+INSERT INTO public.urls VALUES (6, '5y2rx_Zn', 'https://www.toyota.com', 8, 2, '2023-03-03 02:23:18.915201');
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Suzan', 'suzan@gmail.com', '$2b$10$7djd2RgOQpFmFpvNoJVfmephBKhcHVDnuZXE89leraNuRqlEQAqVS', '650c6554-6cbc-48d4-ab6d-f2b387316860');
-INSERT INTO public.users VALUES (2, 'Gerson', 'gerson@gmail.com', '$2b$10$MDKLzsy8j4FajxHhn4Mub.jAQTo92QezIUf5ea1gw86Ux6VXxGigG', '8c4a9538-3283-4fb7-aea3-146dad5cf0dc');
-INSERT INTO public.users VALUES (3, 'Rafaela', 'rafa@gmail.com', '$2b$10$CizziJfJQh1epVQJ2GVZ/.ecZOqzQJt4bh6hKle9snfVdxmkn4Rs.', 'e9d41e1a-8cc2-4638-8656-869b9e513742');
-INSERT INTO public.users VALUES (5, 'Pedro', 'pedro@gmail.com', '$2b$10$CoJf4FiK7QTBWsobMNX7EuGmi8ugVfY8KHhTZqqUP/Pmvky47m6Jy', '200dd83a-239b-4a58-9505-635b7695888a');
-INSERT INTO public.users VALUES (4, 'Bruno', 'bruno@gmail.com', '$2b$10$HNAxjKtfsPwplQKb8jPtUeJomTV9Dsjd7fuY7HJKq/8iC3bQ6DC..', 'a8284ac8-ca7f-4025-a105-5177bff8bc80');
+INSERT INTO public.users VALUES (1, 'Gerson', 'gerson@email.com', '$2b$10$Abx0ZzH04oJ/s8JIXGuVjOj2W0hcwN6.scQSk7Wjn64FunFnJtphi', 'c01e13bf-b43c-4b48-aad4-5292c80f0f62', '2023-03-03 02:11:28.424745');
+INSERT INTO public.users VALUES (2, 'rafaela', 'rafaela@email.com', '$2b$10$CMAiCnscOeFvZgQtVJoMb.ZeOz2MF/Yv9Shfm/HMibD.Dl/PGJaXe', '9125d68b-ceaf-4b40-a738-0530a2ea3ebc', '2023-03-03 02:21:54.259633');
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 5, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 6, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 5, true);
+SELECT pg_catalog.setval('public.users_id_seq', 2, true);
 
 
 --
