b5a4ad7972562d5e7ef905a20314cc4b99adebbc
GersonGoncalves <gerson.devpro@gmail.com>
2023-03-03 13:40:55 -0300

    nova mudança no db

--- database/db.js
+++ database/db.js
@@ -6,11 +6,8 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-  ...(process.env.MODE === "prod" && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
 };
+
+if (process.env.MODE === "prod") configDatabase.ssl = true
 
 export const db = new Pool(configDatabase);
